42eeeb1469721c81f01bd588a0b680b93ef7aabb
Misha <misha@iMac-iMac.local>
2022-09-04 14:52:46 +0300

    добавил вторую задачу

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -1,18 +1,18 @@
 ﻿
-int a = 11;
-int b = 50;
+// int a = 11;
+// int b = 50;
 
-if (a > b){ 
-    System.Console.WriteLine("Число А больше числа Б");
-}
-else{
-if (b > a){
-     System.Console.WriteLine("Число Б больше числа А");
-}
-else{
- System.Console.WriteLine("Числа рывны");
-}
-}
+// if (a > b){ 
+//     System.Console.WriteLine("Число А больше числа Б");
+// }
+// else{
+// if (b > a){
+//      System.Console.WriteLine("Число Б больше числа А");
+// }
+// else{
+//  System.Console.WriteLine("Числа рывны");
+// }
+// }
 
 
 int a = 5;
