e141b9f32e9e49102ce224cb5c18339203be9517
Misha <misha@iMac-iMac.local>
2022-09-04 11:29:00 +0300

    задача 1

---
+++ Fifst_seminar/home_work02/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int a = 11;
+int b = 50;
+
+if (a > b){ 
+    System.Console.WriteLine("Число А больше числа Б");
+}
+else{
+if (b > a){
+     System.Console.WriteLine("Число Б больше числа А");
+}
+else{
+ System.Console.WriteLine("Числа рывны");
+}
+}
