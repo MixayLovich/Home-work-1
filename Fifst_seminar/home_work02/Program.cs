827a738e00d6f23630d4a0a9058fffb37b56c330
Misha <misha@iMac-iMac.local>
2022-09-07 16:30:56 +0300

    добавил вторую задачу ко второму семинару

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -34,7 +34,21 @@
 
 
 
-int n = 648;
+// int n = 648;
  
-System.Console.WriteLine(n / 10 % 10);
+// System.Console.WriteLine(n / 10 % 10);
 
+
+
+
+int n = new Random().Next(100, 10000);
+
+if (n < 100){
+
+ System.Console.WriteLine("Третьей цифры нет");
+}
+else{
+    int result = n / 100 % 10;
+    System.Console.WriteLine($"Третья цифра числа {n} -> {result}");
+
+}
