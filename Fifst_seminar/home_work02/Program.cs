26fe2c7c22a95c80c1f71fcf80e997d01f461e9f
Misha <misha@iMac-iMac.local>
2022-09-12 22:56:09 +0300

    добавил первую задачу к третьему семинаруу

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -54,11 +54,38 @@
 // }
 
 
-int day =3;
+// int day =3;
 
-if (day < 1 || day > 7){
-System.Console.WriteLine("Неверный день недели");
+// if (day < 1 || day > 7){
+// System.Console.WriteLine("Неверный день недели");
+// }
+// else if (day > 5 && day < 8){
+//     System.Console.WriteLine("Это выходной");
+// }
+
+
+
+System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int d1 = n / 10000;
+n -= d1 * 10000;
+
+ int d2 = n / 1000;
+ n -= d2 * 1000;
+
+int d3 = n / 100;
+n -= d3 * 100;
+
+int d4 = n / 10;
+n -= d4 * 10;
+
+int d5 = n % 10;
+
+if (d1 == d5 && d2 == d4){
+    System.Console.WriteLine("палиндром");
 }
-else if (day > 5 && day < 8){
-    System.Console.WriteLine("Это выходной");
+else{
+    System.Console.WriteLine("не палиндром");
+    
 }
