c79e3d37afcc8f5e6c2f82e6d7b32411f2775b88
Misha <misha@iMac-iMac.local>
2022-09-12 23:45:07 +0300

    добавил вторую задачу к третьему семинаруу

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -65,27 +65,39 @@
 
 
 
-System.Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int d1 = n / 10000;
-n -= d1 * 10000;
+// int d1 = n / 10000;
+// n -= d1 * 10000;
 
- int d2 = n / 1000;
- n -= d2 * 1000;
+//  int d2 = n / 1000;
+//  n -= d2 * 1000;
 
-int d3 = n / 100;
-n -= d3 * 100;
+// int d3 = n / 100;
+// n -= d3 * 100;
 
-int d4 = n / 10;
-n -= d4 * 10;
+// int d4 = n / 10;
+// n -= d4 * 10;
 
-int d5 = n % 10;
+// int d5 = n % 10;
 
-if (d1 == d5 && d2 == d4){
-    System.Console.WriteLine("палиндром");
-}
-else{
-    System.Console.WriteLine("не палиндром");
+// if (d1 == d5 && d2 == d4){
+//     System.Console.WriteLine("палиндром");
+// }
+// else{
+//     System.Console.WriteLine("не палиндром");
     
-}
+
+
+int x1 = 3;
+int y1 = 6;
+int z1 = 8;
+
+int x2 =2;
+int y2 =1;
+int z2 = -7;
+
+double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+System.Console.WriteLine(distance);
