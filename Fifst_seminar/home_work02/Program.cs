068ba4ea82d86c3593f539b81911e1bc795415ec
Misha <misha@iMac-iMac.local>
2022-09-25 16:04:31 +0300

    добавил задачу к червертому семинару

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -114,4 +114,38 @@
 
 
 
+ // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+ // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+ // 6, 1, 33 -> [6, 1, 33]
+
+
+  int[] GetArray()
+  {
+      int[] result = new int[8];
+
+      for (int i = 0; i < result.Length; i++)
+      {
+          result[i] = new Random().Next(100);
+      }
+
+      return result;
+  }
+
+  void PrintArray(int[] arr)
+  {
+      int i = 0;
+      Console.Write("[");
+      while (i < arr.Length)
+      {
+          Console.Write(arr[i]);
+          i++;
+          if (i < arr.Length)
+          {
+              Console.Write(", ");
+          }
+      }
+     Console.Write("]");
+  }
+
+  PrintArray(GetArray());
 
