fab7b108fe2eb096c26d5ba51c5053472c9164fe
Misha <misha@iMac-iMac.local>
2022-09-28 16:22:13 +0300

    добавил задачу 47 к седьмому семинару

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -246,3 +246,48 @@
 //      int[] crossPoint = GetCrossPoint(lineData1, lineData2);
 //      Console.Write($"Точка пересечения прямых: ({crossPoint[X_COORD]}; {crossPoint[Y_COORD]})");
 //  }
+
+
+/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+ m = 3, n = 4.
+ 0,5 7 -2 -0,2
+ 1 -3,3 8 -9,9
+ 8 7,8 -7,1 9 */
+
+ // int Prompt(string message)
+ // {
+ //     Console.Write(message);
+ //     int result = Convert.ToInt32(Console.ReadLine());
+ //     return result;
+ // }
+
+ // double[,] GenerateArray(int rows, int columns)
+ // {
+ //     double[,] array = new double[rows, columns];
+ //     for (int i = 0; i < array.GetLength(0); i++)
+ //     {
+ //         for (int j = 0; j < array.GetLength(1); j++)
+ //         {
+ //             array[i, j] = new Random().NextDouble() * 20 - 10; // для диапазона чисел от (-10, 10)
+ //         }
+ //     }
+ //     return array;
+ // }
+
+ // void PrintArray(double[,] matrix)
+ // {
+ //     for (int i = 0; i < matrix.GetLength(0); i++)
+ //     {
+ //         for (int j = 0; j < matrix.GetLength(1); j++)
+ //         {
+ //             System.Console.Write($"{matrix[i, j]:f1}\t");
+ //         }
+ //         System.Console.WriteLine();
+ //     }
+ //     System.Console.WriteLine();
+ // }
+
+ // int countRows = Prompt("Введите количество строк > ");
+ // int countColumns = Prompt("Введите количество столбцов > ");
+ // double[,] myArray = GenerateArray(countRows, countColumns);
+ // PrintArray(myArray);
