3332542fb880712e3168a1078dabc428e9ad11db
Misha <misha@iMac-iMac.local>
2022-10-04 16:16:08 +0300

    добавил задачу 64 к финальному проекту

--- Fifst_seminar/home_work02/Program.cs
+++ Fifst_seminar/home_work02/Program.cs
@@ -354,3 +354,30 @@
  // int[,] myarray = GenerateArray(rows: 3, columns: 2, minRnd: 0, maxRnd: 10);
  // PrintArray(myarray);
  // System.Console.WriteLine($"В данном массиве, строка {MinSumElementsRow(myarray)+1} это строка с наименьшей суммой элементов");
+
+
+
+
+/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+ N = 5 -> "5, 4, 3, 2, 1"
+ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+ // int Prompt(string message)
+ // {
+ //     System.Console.Write(message);
+ //     return Convert.ToInt32(Console.ReadLine());
+ // }
+
+ // void ShowNumbers(int number)
+ // {
+ //     if (number <= 0)
+ //     {
+ //         return;
+ //     }
+ //     System.Console.Write($"{number} ");
+ //     ShowNumbers(number - 1);
+ // }
+
+ // int number = Prompt("Введите номер >");
+ // System.Console.Write($"N = {number} -> ");
+ // ShowNumbers(number);
